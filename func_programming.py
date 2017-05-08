61140dc28a57472b6b7fe10f3006a517de994341
Yi Li <liyi64125837@163.com>
2017-05-07 22:20:11 -0400

    added func_programming.py

---
+++ func_programming.py
@@ -0,0 +1,15 @@
+def toUpper(arg):
+    return arg.upper()
+names = map(toUpper, ["test", "Mary", "TERRY", "kevin"])
+print names
+
+words = map(lambda x: x.upper(), ["test", "Mary", "TERRY", "kevin"])
+print words
+
+squares = map(lambda x: x * x, [1,3,5,7,9])
+print squares
+
+num = [1, -10, 5, -21, 16, -33, 14]
+positive_num = filter(lambda x: x > 0, num)
+avg = reduce(lambda x, y: x + y, positive_num) / len(positive_num)
+print avg
