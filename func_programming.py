2dea0d7d262182e0934c27b2426444705f429c0e
Yi Li <liyi64125837@163.com>
2017-05-07 22:45:47 -0400

    added func_programming.py

--- func_programming.py
+++ func_programming.py
@@ -13,3 +13,11 @@ num = [1, -10, 5, -21, 16, -33, 14]
 positive_num = filter(lambda x: x > 0, num)
 avg = reduce(lambda x, y: x + y, positive_num) / len(positive_num)
 print avg
+
+tags = ["oscar", "epl", "presidentelection"]
+tags = [t.upper() for t in tags]
+print tags
+
+tags = ["oscar", "epl", "presidentelection"]
+upper_tages = map(lambda x: x.upper(), tags)
+print upper_tages
